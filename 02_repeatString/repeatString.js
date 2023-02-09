4e37ab09a724582ebc578f291e1f30a0866beb99
copaiement <cole.paiement@gmail.com>
2023-02-09 10:28:40 -0800

    Update repeatString exercise to pass included test. Update test file to run all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let pattern = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i ++) {
+        pattern += string;
+    }
+    return pattern;
 };
 
 // Do not edit below this line
