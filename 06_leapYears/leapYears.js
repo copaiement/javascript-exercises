9b58692cd59c200b14b7b388bac7732e73cb1ae2
copaiement <cole.paiement@gmail.com>
2023-02-09 12:20:53 -0800

    Update leapYears to give correct output. Update spec to run all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // test for divisibility, biggest first
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
