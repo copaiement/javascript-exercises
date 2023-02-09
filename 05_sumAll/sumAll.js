da9e940c97baa7cbf22d5dd6199a4d08569afa69
copaiement <cole.paiement@gmail.com>
2023-02-09 12:04:13 -0800

    Update sumAll to clean input and do sum. Update spec to run all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(input1, input2) {
+    // initialize variables
+    let start = 0;
+    let end = 0;
+    let sum = 0;
+    // clean input values
+    if (input1 < 0 || 
+        input2 <0 ||
+        typeof(input1) !== 'number' ||
+        typeof(input2) !== 'number') {
+        return 'ERROR';
+    }
+
+    // set start and stop regardless of order
+    if (input1 > input2) {
+        start = input2;
+        end = input1;
+    } else {
+        start = input1;
+        end = input2;
+    }
+
+    for (let i = start; i <= end; i ++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
