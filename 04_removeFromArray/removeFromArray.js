8db3e5ade695f926577382362d33721113b8b354
copaiement <cole.paiement@gmail.com>
2023-02-09 11:34:27 -0800

    Update removeFromArray with working function to parse input. Update spec to run all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...input) {
+    // create array from first input argument
+    let array = input[0];
+    // loop through the rest of the input arguments
+    for (let arg = 1; arg < input.length; arg++) {    
+        // check each arg against the input array
+        for (i = 0; i < array.length; i++) {
+            if (array[i] === input[arg]) {
+                let removed = array.splice(i,1);
+            }
+        }
+    }    
+    return array;
 };
 
 // Do not edit below this line
