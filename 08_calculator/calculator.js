7e31815ec04ae88421af0053893dfd77dd5c7088
copaiement <cole.paiement@gmail.com>
2023-02-21 15:15:17 -0800

    Update calculator.js with functions. Update spec to run all tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+//add two numbers
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+//subtract two numbers
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+//give sum of array
+const sum = function(array) {
+	const total = array.reduce((sum, num) => {
+    return (sum + num);
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
+//multiply an array
 
+//original code.
+
+// const multiply = function(array) {
+//   const product = array.reduce((total, num) => {
+//     return (total * num);
+//   }, 1);
+//   return product;
+// };
+
+//multiply proper solution. 
+const multiply = function(array) {
+  return array.length
+    ? array.reduce((total, num) => total * num)
+    : 0;
 };
 
-const power = function() {
-	
+//one number to the power of another
+const power = function(num1, num2) {
+	const power = num1 ** num2;
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  if (num === 0) {
+    return fact;
+  } else {
+    while (num > 1) {
+      fact *= num;
+      num --;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
