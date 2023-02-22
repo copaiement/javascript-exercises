284a683dc88b54d7e5217073e90c5d4f8bf92107
copaiement <cole.paiement@gmail.com>
2023-02-22 10:06:42 -0800

    Update code to create fib sequence. Update spec to run all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    //each entry in the series is the sum of the previous two entries
+    //1, 1, 2, 3, 5, 8, 13, 21, 34, 55, etc
+
+    //convert to number
+    index = +index;
+
+    //reject negatives
+    if (index < 0) {
+        return "OOPS";
+    }
+
+    //start with first two numbers in series
+    const fibo = [1, 1];
+
+    //after that can calculate based on the number selected
+    for (let i = 0; i < index; i++) {
+        //check if index exists
+        if (fibo[i] === undefined) {
+            fibo[i] = fibo[i-2] + fibo[i-1];
+        }
+    }
+
+    //return the value at the desired index (but we start from 0)
+    return fibo[(index-1)];
 
 };
+
+//to optimize this we might want to store the current index value in a 
+//global variable and then only update the array if the index is
+//higher than the current vector length. 
 
 // Do not edit below this line
 module.exports = fibonacci;
