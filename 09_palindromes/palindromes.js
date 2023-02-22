f9c7377c1dc6a76f1d4c129c070186b14bbfcafe
copaiement <cole.paiement@gmail.com>
2023-02-22 10:07:32 -0800

    Update palindromes code and run all spec tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //create string of all letters with no spaces, punctuation, or caps
+    string = string.toLowerCase();
+    let cleaned = "";
+    for (let i = 0; i < string.length; i++) {
+        cleaned = (/^[a-z]*$/.test(string[i]))
+                    ? cleaned + string[i]
+                    : cleaned;
+    }
+    
+    let cleanedBack = "";
+
+    //reverse string
+    for (let j = (cleaned.length-1); j >= 0; j--) {
+        cleanedBack += cleaned[j];
+    }
+
+    return (cleaned === cleanedBack);
 
 };
 
