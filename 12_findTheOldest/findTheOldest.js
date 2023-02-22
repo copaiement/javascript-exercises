8bc60e2901c1217dcd18bd814004704250253c7e
copaiement <cole.paiement@gmail.com>
2023-02-22 11:34:42 -0800

    Add function to find the oldest person. update spec to run all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
 
+    //check age. if death year dne, use date function to get this year
+
+    //sort to get oldest first
+    const oldest = peopleArray.sort(function(a,b) {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        } else if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+
+        return (aAge > bAge ? -1 : 1);
+
+    });
+
+    //return first person object
+    return oldest[0];
 };
+
+// sample input
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
 // Do not edit below this line
 module.exports = findTheOldest;
